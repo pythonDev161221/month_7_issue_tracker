62dcc32f4cdd1e7f7f3b59fb4debc54345ff2758
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-12 17:47:00 +0600

    create models in models.py  and registrate them in admin.py  and renew dump.json

--- issue_tracker/webapp/admin.py
+++ issue_tracker/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Issue, Type, Status
 
 # Register your models here.
+admin.site.register(Issue)
+admin.site.register(Type)
+admin.site.register(Status)
--- issue_tracker/webapp/models.py
+++ issue_tracker/webapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BaseStr(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Type(BaseStr):
+    pass
+
+
+class Status(BaseStr):
+    pass
+
+
+class Issue(models.Model):
+    summary = models.CharField(max_length=200, null=False, blank=False)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT)
+    type = models.ForeignKey('webapp.Type', on_delete=models.PROTECT)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.summary}'
