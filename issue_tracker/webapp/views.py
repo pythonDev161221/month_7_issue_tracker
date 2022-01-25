cb2fb0ba314e527f01a548eae44c4c69052ddfcc
pythonDev161221 <adverbekreklamov@gmail.com>
2022-01-25 20:39:03 +0600

    pagination in index.html now works good.

--- issue_tracker/webapp/views.py
+++ issue_tracker/webapp/views.py
@@ -31,27 +31,32 @@ class IndexView(ListView):
     paginate_by = 10
     paginate_orphans = 2
 
-    # def get(self, request, *args, **kwargs):
-    #     form = self.get_form()
-    #     search_value = self.get_search_value()
-    #     return super().get(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.GET.get('search'):
+            query = Q(summary__icontains=self.request.GET.get('search')) | \
+                    Q(description__icontains=self.request.GET.get('search'))
+            queryset = queryset.filter(query)
+        return queryset
 
     def get_context_data(self, *, object_list=None, **kwargs):
         kwargs = super().get_context_data(object_list=object_list, **kwargs)
         kwargs['form'] = SearchForm()
-        if self.request.GET.get('search'):
-            issues = Issue.objects.filter(
-                Q(summary__icontains=self.request.GET.get('search')) |
-                Q(description__icontains=self.request.GET.get('search')))
-            kwargs['issues'] = issues
+        if self.search_value:
+            kwargs['search'] = self.search_value
         return kwargs
 
-    # def get_form(self):
-    #     return SearchForm(self.request.GET)
+    def get_form(self):
+        return SearchForm(self.request.GET)
 
-    # def get_search_value(self):
-    #     if self.form.is_valid:
-    #         return self.form.cleaned_data.get('search')
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data.get('search')
 
 
 class IssueView(TemplateView):
