3830c9d219f4f1a4b6f68501b9f91fe81f83d5be
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-08 20:14:04 +0600

    add new app accounts to settings

--- issue_tracker/accounts/urls.py
+++ issue_tracker/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from webapp.views import ProjectListView
 
-
+app_name = "accounts"
 
 urlpatterns = [
-    # path('', ProjectListView.as_view(), name='project_list_view'),
+    path('', ProjectListView.as_view(), name='project_list_view'),
 ]
