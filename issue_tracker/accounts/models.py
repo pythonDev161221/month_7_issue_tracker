21ac9503680b03040445c60a670831e18396c61b
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-18 23:19:56 +0600

    pip3 install Pillow and add Profile model to accounts.models.py

--- issue_tracker/accounts/models.py
+++ issue_tracker/accounts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        related_name="profile",
+        verbose_name="Профиль",
+        on_delete=models.CASCADE,
+    )
+    github = models.URLField(
+        max_length=200,
+        null=True,
+        blank=False,
+        verbose_name="ссылка на github",
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=True,
+        blank=False,
+        verbose_name="О себе",
+    )
+    avatar = models.ImageField(
+        verbose_name="Аватар",
+        upload_to="avatars/",
+        null=True,
+        blank=False,
+    )
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+    def __str__(self):
+        return f"{self.user.username}. {self.id}"
