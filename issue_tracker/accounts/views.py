1e65f0a721bd51b6e89537795fe3f7bc446606df
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-20 17:22:29 +0600

    some tiny improvements

--- issue_tracker/accounts/views.py
+++ issue_tracker/accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth import login, get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
