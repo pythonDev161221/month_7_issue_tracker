8facb829af5c0b2b8d1e5037af5b8300fa12bc12
pythonDev161221 <adverbekreklamov@gmail.com>
2022-02-24 18:29:58 +0600

    some simplifi in views/issues permissions

--- issue_tracker/webapp/views/issues.py
+++ issue_tracker/webapp/views/issues.py
@@ -10,45 +10,6 @@ from webapp.forms import IssueForm
 from webapp.models import Issue, Project
 
 
-# class CreateIssueView(View):
-#     def get(self, request, *args, **kwargs):
-#         context = {}
-#         form = IssueForm()
-#         context['form'] = form
-#         return render(request, "issues/create_issue_view.html", context)
-#
-#     def post(self, request, *args, **kwargs):
-#         form = IssueForm(data=request.POST)
-#         if form.is_valid():
-#             summary = form.cleaned_data.get('summary')
-#             author = self.request.user
-#             description = form.cleaned_data.get('description')
-#             status = form.cleaned_data.get('status')
-#             project = Project.objects.get(pk=self.kwargs.get('project_pk'))
-#             is_deleted = False
-#             type_names = form.cleaned_data.get('type_names')
-#             # print(summary, author, description, status, project, is_deleted, type_names)
-#             print(summary)
-#             print(author)
-#             print(description)
-#             print(status)
-#             print(project)
-#             print(is_deleted)
-#             print(type_names)
-#             new_issue = Issue(summary=summary, author=author,
-#                               description=description, status=status, project=project,
-#                               is_deleted=is_deleted)
-#             new_issue.type_names.add(type_names)
-#             new_issue.save()
-#             return redirect("webapp:project_detail_view", pk=self.kwargs.get('project_pk'))
-#         return redirect("webapp:project_list_view")
-#
-#         # issue = Issue.objects.create()
-#         # issue.project = Project.objects.get(pk=self.kwargs.get('project_pk'))
-#
-#
-#         # return render(request, "issues/create_issue_view.html", context)
-
 class CreateIssueView(PermissionRequiredMixin, CreateView):
     model = Issue
     template_name = 'issues/create_issue_view.html'
@@ -58,7 +19,6 @@ class CreateIssueView(PermissionRequiredMixin, CreateView):
     def has_permission(self):
         project = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
         return super().has_permission() and self.request.user in project.users.all()
-
 
     def form_valid(self, form):
         project = get_object_or_404(Project, pk=self.kwargs.get('project_pk'))
@@ -88,17 +48,6 @@ class IssueUpdateView(PermissionRequiredMixin, UpdateView):
                        kwargs={'project_pk': self.kwargs.get('project_pk')})
 
 
-# class IssueDeleteView(View):
-#     def get(self, request, *args, **kwargs):
-#         issue = get_object_or_404(Issue, pk=kwargs.get('issue_pk'))
-#         project_pk = kwargs.get('project_pk')
-#         return render(request, 'issues/issue_delete.html', {'issue': issue, 'project_pk': project_pk})
-#
-#     def post(self, request, *args, **kwargs):
-#         issue = get_object_or_404(Issue, pk=kwargs.get('issue_pk'))
-#         issue.delete()
-#         return redirect('project_list_view')
-
 class IssueDeleteView(PermissionRequiredMixin, DeleteView):
     template_name = 'issues/issue_delete.html'
     model = Issue
@@ -106,8 +55,7 @@ class IssueDeleteView(PermissionRequiredMixin, DeleteView):
     permission_required = 'webapp.delete_issue'
 
     def has_permission(self):
-        return super().has_permission() and \
-               any(user == self.request.user for user in self.get_object().project.users.all())
+        return super().has_permission() and self.request.user in self.get_object().project.users.all()
 
     def form_valid(self, form):
         success_url = self.get_success_url()
